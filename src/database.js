2584fb208b6ba052952167a240b1c88e2aac3876
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-17 10:48:30 -0500

    Set .env file to connect to mongodb through mlab

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const URI = process.env.MONGODB_URI
-  ? process.env.MONGODB_URI
-  : "mongodb://localhost:27017/database";
+const URI = process.env.MONGOLAB_URI
+  ? process.env.MONGOLAB_URI
+  : "mongodb://localhost:27017/mernDb";
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
